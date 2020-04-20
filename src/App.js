45804b74fb6e1ceb623d389fb2f47f41a5719aff
Gerardo Quiñonez <gerardo01302011@gmail.com>
2020-04-20 15:40:09 -0600

    Add Code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//State del Formulario
+	const [busqueda, guardarBusqueda] = useState({
+		ciudad: "",
+		pais: "",
+	});
+
+	//Otro State
+	const [consultar, guardarConsultar] = useState(false);
+
+	const [resultado, guardarResultado] = useState({});
+
+	//State del error al no encontrar ciudad
+	const [error, guardarError] = useState(false);
+
+	//Desctructuring ciudad y pais de la busqueda
+	const { ciudad, pais } = busqueda;
+
+	useEffect(() => {
+		//Funcion para consultar la API
+		const consultarAPI = async () => {
+			if (consultar) {
+				const appId = "6c525df7efc49e7325e0c2cb776a87fa";
+				const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+				const respuesta = await fetch(url);
+				const resultado = await respuesta.json();
+
+				guardarResultado(resultado);
+
+				//Para que podamos hacer mas de una consulta
+				guardarConsultar(false);
+
+				//Si no encuentra esa ciudad
+				if (resultado.cod === "404") {
+					guardarError(true);
+				} else {
+					guardarError(false);
+				}
+			}
+		};
+
+		consultarAPI();
+		// eslint-disable-next-line
+	}, [consultar]);
+
+	let componente;
+	if (error) {
+		componente = <Error mensaje="No hay resultados disponibles" />;
+	} else {
+		componente = <Clima resultado={resultado} />;
+	}
+
+	return (
+		<Fragment>
+			<Header titulo="Clima React App" />
+			<div className="contenedor-form">
+				<div className="container">
+					<div className="row">
+						<div className="col m6 s12">
+							<Formulario
+								busqueda={busqueda}
+								guardarBusqueda={guardarBusqueda}
+								guardarConsultar={guardarConsultar}
+							/>
+						</div>
+						<div className="col m6 s12">{componente}</div>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default App;
